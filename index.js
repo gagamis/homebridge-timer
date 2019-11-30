8d07e5b91319ff70fbe6cf474de611e901df7b0b
Mihály GÁL <mihaly.gal.gamis@gmail.com>
2019-11-30 14:09:37 +0100

    refactoring 1

--- index.js
+++ index.js
@@ -7,44 +7,54 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
-
+  ContactState = homebridge.hap.Characteristic.ContactSensorState;
   homebridge.registerAccessory('homebridge-watchdog', 'WatchDog', watchDog);
 };
 
 function watchDog(log, config) {
   this.log = log;
   this.name = config['name'] || 'Gamis watchdog';
-  this.period = config['period'] || 60000;
+  this.interval = config['interval'] || 60000;
   this.contactTriggered = false;
+
+  setInterval(this.watchdogContactState.bind(this), this.interval);
 }
 
-watchDog.prototype.getServices = function() {
-  // information service
-  var informationService = new Service.AccessoryInformation();
-  informationService
-    .setCharacteristic(Characteristic.Manufacturer, 'Gamis Manufacturer')
-    .setCharacteristic(Characteristic.Model, 'Rev-1')
-    .setCharacteristic(Characteristic.SerialNumber, 'A1S2NASF88EW');
+watchDog.prototype = {
+  identify: function(callback) {
+    callback(null);
+  },
 
-  // contact service
-  this.contactService = new Service.ContactSensor(this.name);
-  this.contactService
-    .getCharacteristic(Characteristic.ContactSensorState)
-    .on('get', this.getContact.bind(this));
+  watchdogContactState: function() {
+    this.contactTriggered = !this.contactTriggered;
+    this.service
+      .getCharacteristic(Characteristic.ContactSensorState)
+      .setValue(this.contactTriggered);
+  },
 
-  var services = [informationService, this.contactService];
-  return services;
+  getContactSensorState: function(callback) {
+    this.log('getContactSensorState: ', this.contactTriggered);
+    callback(null, this.contactTriggered);
+  },
+  getName: function(callback) {
+    callback(null, this.name);
+  },
+  getServices: function() {
+    var informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, 'GAMIS Systems')
+      .setCharacteristic(Characteristic.Model, 'Rev-1')
+      .setCharacteristic(Characteristic.SerialNumber, 'Version 0.0.3')
+      .setCharacteristic(Characteristic.SerialNumber, 'A1S2NASF88EW');
+
+    this.service
+      .getCharacteristic(Characteristic.ContactSensorState)
+      .on('get', this.getContactSensorState.bind(this));
+
+    this.service
+      .getCharacteristic(Characteristic.Name)
+      .on('get', this.getName.bind(this));
+
+    return [informationService, this.service];
+  }
 };
-
-watchDog.prototype.getContact = function(callback) {
-  callback(null, this.contactTriggered);
-};
-
-// timer
-const intervalObj = setInterval(() => {
-  this.contactTriggered = !this.contactTriggered;
-  this.contactService.setCharacteristic(
-    Characteristic.ContactSensorState,
-    this.contactTriggered
-  );
-}, this.period);
